abcbde21078e8d28180bfc221efedaec7ebb1003
George Richardson <george@gjhr.me>
2021-10-02 16:27:36 +0100

    Use correct hugo url

--- .github/scripts/deployhugo.sh
+++ .github/scripts/deployhugo.sh
@@ -1,7 +1,7 @@
 set -ex
 
 echo "Installing hugo"
-curl -fSL "https://releases.hashicorp.com/terraform/0.11.15/terraform_0.11.15_linux_amd64.zip" -o hugo.tar.gz
+curl -fSL "https://github.com/gohugoio/hugo/releases/download/v0.88.1/hugo_extended_0.88.1_Linux-64bit.tar.gz" -o hugo.tar.gz
 tar -xf hugo.tar.gz -C /opt/hugo
 sudo rm /usr/local/bin/hugo
 sudo ln -s /opt/hugo/hugo /usr/local/bin/hugo
