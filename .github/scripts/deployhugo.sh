6f034f12e5ce9c2d9e97b34279e561048a0a0c38
George Richardson <george@gjhr.me>
2021-10-02 16:28:57 +0100

    Create hugo directory

--- .github/scripts/deployhugo.sh
+++ .github/scripts/deployhugo.sh
@@ -2,6 +2,7 @@ set -ex
 
 echo "Installing hugo"
 curl -fSL "https://github.com/gohugoio/hugo/releases/download/v0.88.1/hugo_extended_0.88.1_Linux-64bit.tar.gz" -o hugo.tar.gz
+mkdir -p /opt/hugo
 tar -xf hugo.tar.gz -C /opt/hugo
 sudo rm /usr/local/bin/hugo
 sudo ln -s /opt/hugo/hugo /usr/local/bin/hugo
